1d668ab9416448013068f6c74b8130205c999a62
ntanh <ntanh.hus@gmail.com>
2023-08-22 16:39:19 +0700

    Change code folder to retriever, not reader

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import reader
+import retriever
 
 if __name__ == '__main__':
-    results = reader.find_relevant_documents("thủ đô của Việt Nam là gì?")
+    results = retriever.find_relevant_documents("thủ đô của Việt Nam là gì?")
     print(results)
