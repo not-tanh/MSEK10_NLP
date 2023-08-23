08294865cf2d75fd5847bc151e1bdaad94bd708a
Hai Binh Do <binh_dh@flinters.vn>
2023-08-23 20:23:45 +0700

    feat: add max allowed token count to sent. chunk

--- reader/predictor.py
+++ reader/predictor.py
@@ -5,7 +5,7 @@ from typing import TypedDict, Union
 import torch
 from transformers import AutoTokenizer, PreTrainedTokenizerFast
 from transformers.modeling_outputs import QuestionAnsweringModelOutput
-from underthesea import sent_tokenize
+from underthesea import sent_tokenize, word_tokenize
 
 from .pretrained_model import MRCQuestionAnswering
 from .tokenizer import TokenizeResult, TokenizerWrapper
@@ -49,26 +49,34 @@ def pad_tokens(
 
     for i, v in enumerate(values):
         without_padding = (
-            padded[i][size - len(v):] if left_pad else padded[i][: len(v)]
+            padded[i][size - len(v) :] if left_pad else padded[i][: len(v)]
         )
         copy_tensor(v, without_padding)
     return padded
 
 
-def chunked_by_sentence(text: str, sentence_count=5, skip=0):
+def chunked_by_sentence(text: str, sentence_count=5, max_tokens=500, skip=0):
     assert 0 <= skip < sentence_count
     sentences = sent_tokenize(text)
-    return [
-        " ".join(sentences[i : i + sentence_count])
-        for i in range(0, len(sentences) - sentence_count, skip + 1)
-    ]
+    sentence_tokens = [len(word_tokenize(sentence)) for sentence in sentences]
+    chunk_sentences = list[str]()
+    chunk_count = len(sentences) - sentence_count
+    step = skip + 1
+    for i in range(0, chunk_count, step):
+        chunk_tokens = 0
+        chunk_sentences.clear()
+        for j in range(i, i + sentence_count):
+            token_count = sentence_tokens[j]
+            if token_count + chunk_tokens > max_tokens:
+                break
+            chunk_sentences.append(sentences[j])
+            chunk_tokens += token_count
+        yield " ".join(chunk_sentences)
 
 
-def chunk_input(_input: QuestionContextInput) -> list[QuestionContextInput]:
-    return [
-        {"question": _input["question"], "context": ctx_chunk}
-        for ctx_chunk in chunked_by_sentence(_input["context"])
-    ]
+def chunk_input(_input: QuestionContextInput):
+    for ctx_chunk in chunked_by_sentence(_input["context"]):
+        yield QuestionContextInput(question=_input["question"], context=ctx_chunk)
 
 
 class Predictor:
